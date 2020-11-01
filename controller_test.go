41e5ff85440aab01911f9a99c0f36fc769c9f952
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-11-01 16:18:30 +0800

    fix test

--- controller_test.go
+++ controller_test.go
@@ -12,7 +12,7 @@ import (
 func TestController_NewUniqueIP(t *testing.T) {
 	_, n, _ := net.ParseCIDR("10.244.0.0/16")
 	ctl := NewController(n)
-	for i := 1; i < 258; i++ {
+	for i := 2; i < 258; i++ {
 		assert.Equal(t, ipOr(n.IP, intToIP(uint32(i))), ctl.NewUniqueIP())
 	}
 }
